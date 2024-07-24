6e39f6da05222717d7d9393dcb14290962af0c51
jimefle <jimefle@icloud.com>
2024-07-24 12:28:50 -0300

    implementacion de Formspree

--- alpha-web/src/pages/Contact.jsx
+++ alpha-web/src/pages/Contact.jsx
@@ -3,22 +3,67 @@ import '/src/styles/Contact.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faLocationDot, faEnvelope} from '@fortawesome/free-solid-svg-icons';
 import { faWhatsapp} from '@fortawesome/free-brands-svg-icons';
+import { useForm, ValidationError } from '@formspree/react';
+
+function ContactForm() {
+  const [state, handleSubmit] = useForm("mldrgoqa");
+
+  if (state.succeeded) {
+      return <p>Mensaje enviado!</p>;
+  }
+
+  return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="name">Nombre</label>
+            <input
+                id="name"
+                type="text" 
+                name="name"
+                required
+                maxLength={50}
+                placeholder='Nombre'
+            />
+            <label htmlFor="email">Email</label>
+            <input
+                type="email"
+                id="email"
+                name="email"
+                required
+                placeholder="Email@ejemplo.com"
+                className="input"
+            />
+            <ValidationError
+                prefix="Email"
+                field="email"
+                errors={state.errors}
+                className="validation-error"
+            />
+
+            <label htmlFor="message">Mensaje</label>
+            <textarea
+                id="message"
+                name="message"
+                required
+                placeholder="Escribe tu mensaje"
+                className="textarea"
+            />
+            <ValidationError
+                prefix="Message"
+                field="message"
+                errors={state.errors}
+                className="validation-error"
+            />
+
+            <button type="submit" disabled={state.submitting} className="button">Enviar</button>
+        </form>
+  );
+}
+
 const Contact = () => {
     return(
         <div className="contact-page">
             <section className="user-info">
-                <form action="https://formspree.io/contacto@alphascope.com" method="POST">
-                    <label htmlFor="name">Nombre:</label>
-                    <input type="text" id="name" name="name" required />
-                    
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" required />
-                    
-                    <label htmlFor="message">Mensaje:</label>
-                    <textarea id="message" name="message" required></textarea>
-                    
-                    <button type="submit">Enviar</button>
-                </form>
+                <ContactForm ></ContactForm>
             </section>
             <section className="alpha-info">   
                 <div>
