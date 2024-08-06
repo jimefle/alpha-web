6b7418b03c726daebf0857ea1103572c1f3d0fdb
jimefle <jimefle@icloud.com>
2024-08-06 12:45:02 -0300

    delete pages

--- alpha-web/src/components/Header.jsx
+++ alpha-web/src/components/Header.jsx
@@ -34,9 +34,7 @@ const Header = () => {
           <div className='nav-links'>
             <Link to="/" className={getLinkClass("/")}>Inicio</Link>
             <Link to="/products" className={getLinkClass("/products")}>Productos</Link>
-            <Link to="/services" className={getLinkClass("/services")}>Servicios</Link>
             <Link to="/blog" className={getLinkClass("/blog")}>Blog</Link>
-            <Link to="/about-us" className={getLinkClass("/about-us")}>Empresa</Link>
             <Link to="/contact" className={getLinkClass("/contact")}>Contacto</Link>
           </div>
       </header>
