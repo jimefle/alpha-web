a20afea1b79d91f33ce0bfdd254de89bc11eead9
jimefle <jimefle@icloud.com>
2024-08-05 21:07:59 -0300

    flip hover cards mv

--- alpha-web/src/components/Hero.jsx
+++ alpha-web/src/components/Hero.jsx
@@ -1,78 +1,55 @@
 import '/src/styles/Hero.css'
-import { Link } from 'react-router-dom'
-import { useEffect, useRef } from 'react';
+import Article from '../components/Article'
 
 export default function Hero() {
 
-    const UnderlinedHeader = () => {
-        const h5Ref = useRef(null);
-    
-        useEffect(() => {
-            const observer = new IntersectionObserver(
-                ([entry]) => {
-                    if (entry.isIntersecting) {
-                        entry.target.classList.add('show');
-                    } else{
-                        entry.target.classList.remove('show')
-                    }
-                },
-                { threshold: 1 } // Ajusta el umbral según sea necesario
-            );
-    
-            if (h5Ref.current) {
-                observer.observe(h5Ref.current);
-            }
-    
-            return () => {
-                if (h5Ref.current) {
-                    observer.unobserve(h5Ref.current);
-                }
-            };
-        }, []);
-    
-        return <h5 ref={h5Ref} className="h5-underline">SOMOS ESPECIALISTAS EN ENDOSCOPIA VETERINARIA E INDUSTRIAL</h5>;
-    };
     return (
         <section>
             <section className='main-section'>
-                <h1>ALPHASCOPE</h1>
-                <UnderlinedHeader />
+            <Article title=""
+                text={"Somos especialistas en endoscopía veterinaria e industrial/nEntendemos la importancia para los profesionales veterinarios de contar con equipos confiables para obtener los mejores resultados que les permitan realizar su trabajo enfocándose en sus pacientes, delegando en nosotros la atención de sus equipos e instrumental de trabajo./nEn el rubro industrial, proveemos equipos como boroscopios y cámaras de inspección de tuberías, satisfaciendo una amplia gama de necesidades en el ámbito de las inspecciones técnicas no destructivas."}
+                image="./src/image/alpha-image.jpg"
+            />
+
+            <Article title={"Servicios"}
+                    text={"Asesoría y reparación especializada a tu alcance/nContamos con un departamento técnico para responder sus consultas ya sea al momento de decidir la compra como durante el periodo post-venta./nContamos con profesionales con experiencia en la reparación de equipos médicos lo que nos permite ofrecer un servicio técnico completo, que incluye asistencia técnica inmediata, reparación del equipo y provisión de repuestos de todos los equipos que vendemos."}
+                    image={"./src/image/service.jpg"}/>
+
+            <Article title={"Servicios"}
+                    text={"Asesoría y reparación especializada a tu alcance/nContamos con un departamento técnico para responder sus consultas ya sea al momento de decidir la compra como durante el periodo post-venta./nContamos con profesionales con experiencia en la reparación de equipos médicos lo que nos permite ofrecer un servicio técnico completo, que incluye asistencia técnica inmediata, reparación del equipo y provisión de repuestos de todos los equipos que vendemos."}
+                    image={"./src/image/service.jpg"}/>
+
             </section>
+
+        
+
             <section className="card-section">
-                <article>
-                    <h3>Empresa</h3>
-                    <h6> ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</h6>
-                    <Link to="/about-us" className="cta" preventScrollReset={false}>
-                        <span>Ver más</span>
-                        <svg width="13px" height="10px" viewBox="0 0 13 10">
-                            <path d="M1,5 L11,5"></path>
-                            <polyline points="8 1 12 5 8 9"></polyline>
-                        </svg>
-                    </Link>
+                <article className='card'>
+                    <div className="card-front">
+                        <h3>Visión</h3>
+                    </div>
+                    <div className="card-back">
+                        <h6>Convertirnos en referentes en el mercado de los equipos médicos veterinarios y en el área de la endoscopia industrial, ofreciendo productos y servicios de calidad y al mejor precio.</h6>
+                    </div>
                 </article>
-                <article>
-                    <h3>Servicios</h3>
-                    <h6> ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</h6>
-                    <Link to="/services" className="cta">
-                        <span>Ver más</span>
-                        <svg width="13px" height="10px" viewBox="0 0 13 10">
-                            <path d="M1,5 L11,5"></path>
-                            <polyline points="8 1 12 5 8 9"></polyline>
-                        </svg>
-                    </Link>
-                </article>
-                <article>
-                    <h3>Productos</h3>
-                    <h6> ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</h6>
-                    <Link to="/products" className="cta">
-                        <span>Ver más</span>
-                        <svg width="13px" height="10px" viewBox="0 0 13 10">
-                            <path d="M1,5 L11,5"></path>
-                            <polyline points="8 1 12 5 8 9"></polyline>
-                        </svg>
-                    </Link>
+                <article className='card'>
+                    <div className="card-front">
+                        <h3>Misión</h3>
+                    </div>
+                    <div className="card-back">
+                        <h6>Proveer a los profesionales de los equipos necesarios que les permitan realizar su trabajo de forma eficiente, con equipamiento de vanguardia y asequibles, cuidando todo el proceso de adquisición de sus equipos sabiendo que en todo momento cuentan con el apoyo técnico necesario.</h6>
+                    </div>
                 </article>
             </section>
         </section>
     )
 }
+
+/*
+<Link to="/about-us" className="cta" preventScrollReset={false}>
+                        <span>Ver más</span>
+                        <svg width="13px" height="10px" viewBox="0 0 13 10">
+                            <path d="M1,5 L11,5"></path>
+                            <polyline points="8 1 12 5 8 9"></polyline>
+                        </svg>
+                    </Link> */
