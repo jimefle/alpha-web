89ea095df68777429dea6a907c09c8c1f5b8e7c5
jimefle <jimefle@icloud.com>
2024-08-19 16:00:32 -0300

    update phone

--- alpha-web/src/App.jsx
+++ alpha-web/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
             </main>
             <Footer />
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
-                <a href="https://api.whatsapp.com/send?phone=3435093106&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%..." className="float" target="_blank">
+                <a href="https://api.whatsapp.com/send?phone=543435726481&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%..." className="float" target="_blank">
                     <i className="fa fa-whatsapp my-float"></i>
                 </a>
         </div>
--- alpha-web/src/components/Footer.jsx
+++ alpha-web/src/components/Footer.jsx
@@ -20,7 +20,7 @@ export default function Footer(){
                     </div>
                     <div className='info'>
                         <FontAwesomeIcon icon={faWhatsapp} className='contact-line'/>
-                        <a href="https://wa.me/543434341791" target="_blank">+54 343 4 341791</a>
+                        <a href="https://wa.me/543435726481" target="_blank">+54 343 5 726481</a>
                     </div>
                     <div className='info'>
                         <FontAwesomeIcon icon={faEnvelope} className='contact-line' />
